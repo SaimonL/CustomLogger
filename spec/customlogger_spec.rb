ae4aad3ccd4ad9008b1693c89a87a4d21aeebf31
Saimon Lovell (CPG) <staysynchronize@gmai.com>
2014-12-04 09:23:31 -0500

    Test adjust

--- spec/customlogger_spec.rb
+++ spec/customlogger_spec.rb
@@ -38,14 +38,14 @@ describe 'with Custom Logger' do
   describe 'with error colors' do
     it 'will have default error colors' do
       expect( CustomLogger.error_colors
-        ).to eq ({ error: '#8b0000', warning: '#ffe9c4', debug: '#2d5a2e',
+        ).to eq ({ error: '#8b0000', warning: '#6d422e', debug: '#2d5a2e',
                    info: '#31708f' })
     end
 
     it 'will allow to set error color' do
       CustomLogger.error_color(:error, 'lovell')
       expect( CustomLogger.error_colors
-      ).to eq ({ error: 'lovell', warning: '#4c2e20', debug: '#2d5a2e',
+      ).to eq ({ error: 'lovell', warning: '#6d422e', debug: '#2d5a2e',
                  info: '#31708f' })
     end
 
